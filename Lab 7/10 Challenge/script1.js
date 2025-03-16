3f3acee4c0c320e13ed3f491e2b2e4e2b3c648f5
Thawatchai67 <riversoon11@gmail.com>
2025-03-16 14:08:30 +0700

    Delete Lab 7 directory

--- Lab 7/10 Challenge/script1.js
+++
@@ -1,7 +0,0 @@
-let button = document.getElementById("updateTextButton");
-
-button.addEventListener("click", function() {
-    let paragraph = document.getElementById("example");
-    paragraph.textContent = "Updated Text Content";
-    alert("Text has been changed!");
-});
--- Lab 7/10 Challenge/script10.js
+++
@@ -1,6 +0,0 @@
-let button = document.getElementById("startAnimation");
-let square = document.getElementById("square");
-
-button.addEventListener("click", function() {
-    square.classList.toggle("animate");
-});
